989637d587064901b07a40ff0f278b6583a6c4c0
Charles Sherman <shermzor@gmail.com>
2013-06-10 12:06:15 +0100

    Added more special message commands

--- src/atrophy/combat/display/ui/MessageBox.java
+++ src/atrophy/combat/display/ui/MessageBox.java
@@ -579,6 +579,8 @@ public class MessageBox extends Crowd{
 
 		private String parseMessage(String message) {
 			message = message.replaceAll("%playername", conversers[0].getName());
+			message = message.replaceAll("%ainame", conversers[1].getName());
+			message = message.replaceAll("%aifaction", conversers[1].getFaction());
 			return message;
 		}
 
