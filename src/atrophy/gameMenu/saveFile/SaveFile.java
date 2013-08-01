c1006a61e9a9e2d74bc09c4c07cea4695539cdf4
Charles Sherman <shermzor@gmail.com>
2013-08-01 14:21:35 +0100

    Removed save file ironman

--- src/atrophy/gameMenu/saveFile/SaveFile.java
+++ src/atrophy/gameMenu/saveFile/SaveFile.java
@@ -1,6 +1,3 @@
-/*
- * 
- */
 package atrophy.gameMenu.saveFile;
 
 import java.io.File;
@@ -18,64 +15,23 @@ import atrophy.gameMenu.ui.ShopManager;
 import atrophy.gameMenu.ui.StashManager;
 import atrophy.gameMenu.ui.WindowManager;
 
-/**
- * The Class SaveFile.
- */
 public class SaveFile implements Serializable{
 
 	public static String saveLocation = "";
 	
-	/**
-	 * The Constant serialVersionUID.
-	 */
 	private static final long serialVersionUID = -9063488604100773309L;
 	
-	/**
-	 * The squad.
-	 */
 	public Squad squad;
-	
-	/**
-	 * The sectors.
-	 */
 	public ArrayList<Sector> sectors;
-	
-	/**
-	 * The stash.
-	 */
 	public ArrayList<String> stash;
-	
-	/**
-	 * The quests.
-	 */
 	public ArrayList<MissionGiver> quests;
-	
-	/**
-	 * The economy effects.
-	 */
 	public ArrayList<String> economyEffects;
-	
-	/**
-	 * The advance.
-	 */
 	public Integer advance;
 	
-	/**
-	 * The shop item level.
-	 */
 	public TechTree techTree;
-	
 	public Set<String> spawnCodes;
-	
 	public String saveURL;
 	
-	/**
-	 * Instantiates a new save file.
-	 *
-	 * @param squad the squad
-	 * @param sectors the sectors
-	 * @param stash the stash
-	 */
 	public SaveFile(Squad squad, Missions missions, ArrayList<Sector> sectors, ArrayList<String> stash, TechTree techTree, Set<String> spawnCodes) {
 		this.advance = squad.getAdvance();
 		this.squad = squad;
@@ -86,14 +42,6 @@ public class SaveFile implements Serializable{
 		this.spawnCodes = spawnCodes;
 	}
 
-	/**
-	 * Save game.
-	 *
-	 * @param file the file
-	 * @param squad the squad
-	 * @param sectors the sectors
-	 * @param stash the stash
-	 */
 	public static void saveGame(File file, Squad squad, Missions missions, ArrayList<Sector> sectors, ArrayList<String> stash, TechTree techTree, Set<String> spawnCodes){
 		SaveFile save = new SaveFile(squad,missions,sectors,stash,techTree,spawnCodes);
 		
@@ -119,18 +67,6 @@ public class SaveFile implements Serializable{
 		}	
 	}
 	
-	/**
-	 * Load game.
-	 *
-	 * @param file the file
-	 * @param stashManager 
-	 * @param mapWar 
-	 * @param squadMenu 
-	 * @param shopManager 
-	 * @param missions 
-	 * @param windowManager 
-	 * @return 
-	 */
 	public static Squad loadGame(File file, StashManager stashManager, MapManager mapWar, ShopManager shopManager, Missions missions, WindowManager windowManager) {
 		
 		SaveFile save = null;
@@ -156,7 +92,7 @@ public class SaveFile implements Serializable{
 		if(save != null){
 			
 			//Delete save file
-			file.delete();
+//			file.delete();
 			
 			stashManager.setItems(save.stash);
 			mapWar.setSectors(save.sectors);
