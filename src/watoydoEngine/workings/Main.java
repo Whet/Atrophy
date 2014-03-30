663cb02073db06c2a153ce07b17e1f01d419903c
Charles Sherman <shermzor@gmail.com>
2014-03-30 21:49:40 +0100

    Added nimbus colour scheme

--- src/watoydoEngine/workings/Main.java
+++ src/watoydoEngine/workings/Main.java
@@ -1,38 +1,39 @@
-/*
- * 
- */
 package watoydoEngine.workings;
 
+import java.awt.Color;
+
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import watoydoEngine.io.ReadWriter;
 import watoydoEngine.workings.displayActivity.SetupWindow;
 
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class Main.
- */
 public class Main{
 	
-	/**
-	 * The main method.
-	 *
-	 * @param args the arguments
-	 */
-	public static void main(String[] args){
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException{
 		@SuppressWarnings("unused")
 		Main main = new Main();
 	}
 	
-	/**
-	 * Instantiates a new main.
-	 */
-	public Main(){
+	public Main() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException{
 		
 		ReadWriter.HOME_LOCATION = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
 		if(ReadWriter.HOME_LOCATION.endsWith("Atrophy.jar"))
 			ReadWriter.HOME_LOCATION = ReadWriter.HOME_LOCATION.substring(0, ReadWriter.HOME_LOCATION.length() - "Atrophy.jar".length());
+		
+		UIManager.put("nimbusBase", new Color(20,20,20));
+		UIManager.put("nimbusBlueGrey", new Color(50, 50, 50));
+		UIManager.put("control", new Color(90, 90, 90));
+		
+		for (LookAndFeelInfo laf:UIManager.getInstalledLookAndFeels()) {
+			if ("Nimbus".equals(laf.getName())) {
+				UIManager.setLookAndFeel(laf.getClassName());
+				break;
+			}
+		}
 		
 		SwingUtilities.invokeLater(new Runnable() {
 			
